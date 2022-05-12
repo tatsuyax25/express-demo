2918268e71411b0e1c367ddfb0c7b37629d34e2c
Miguel Urena <urena_miguel82@outlook.com>
2022-05-12 10:23:33 -0700

    Updated 404

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ app.get('/api/courses', (req,res) => {
 app.get('/api/courses/:id', (req, res) => {
     const course = courses.find(c => c.id === parseInt(req.params.id));
     if (!course) res.status(404).send('The course with the given ID was not found.'); // 404
+    res.send(course);
 });
 
 // PORT 
