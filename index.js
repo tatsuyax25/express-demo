4930edad41a34c535e4f97fc4807e1c2d71dfb39
Miguel Urena <urena_miguel82@outlook.com>
2022-05-12 14:16:15 -0700

    Add DELETE

--- index.js
+++ index.js
@@ -59,6 +59,20 @@ app.put('/api/courses/:id', (req, res) => {
     res.send(course);
 });
 
+app.delete('/api/courses/:id', (req, res) => {
+    // Look up the course
+    // Not existing, return 404
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course) res.status(404).send('The course with the given ID was not found.');
+
+    // Delete
+    const index = courses.indexOf(course);
+    courses.splice(index, 1);
+
+    // Return the same course
+    res.send(course);
+})
+
 function validateCourse(course) {
     // Validate
     
